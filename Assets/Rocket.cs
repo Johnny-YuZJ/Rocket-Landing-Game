8b0ae3d926b843de79e604877e0027998d33b16a
Johnny-Zhuojun-Yu <shanghaiboy1122@hotmail.com>
2020-05-04 22:01:18 -0400

    Finish Rock design and rigidbody

---
+++ Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    // Start is called before the first frame update
+
+    Rigidbody rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) {
+            rigidbody.AddRelativeForce(Vector3.up);
+            print("space/up pressed");
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(Vector3.forward);
+            print("a/left pressed");
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(-Vector3.forward);
+            print("d/right pressed");
+        }
+    }
+}
